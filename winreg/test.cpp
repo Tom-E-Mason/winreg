9d394a7d3347207ab35a42ea7cfcd55d294fbd34
Tom Mason <masonworldt@gmail.com>
2021-04-29 14:15:44 +0100

    added openclose test

--- winreg/test.cpp
+++ winreg/test.cpp
@@ -1,2 +1,20 @@
 
 #include "pch.h"
+
+#include "include/winreg.h"
+
+using namespace std::string_literals;
+
+TEST(winreg_test, openclose)
+{
+    auto subkey{ L"software\\asio"s };
+    auto hkey{ winreg::local_machine.open(subkey, winreg::access::read) };
+
+    EXPECT_TRUE(hkey);
+    EXPECT_TRUE(hkey.is_open());
+
+    hkey.close();
+
+    EXPECT_FALSE(hkey);
+    EXPECT_FALSE(hkey.is_open());
+}
