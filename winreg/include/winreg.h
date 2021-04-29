1c42d899c54e35b411541a0603028f40f1dff068
Tom Mason <masonworldt@gmail.com>
2021-04-29 15:06:10 +0100

    added query_info method

--- winreg/include/winreg.h
+++ winreg/include/winreg.h
@@ -69,6 +69,57 @@ namespace winreg
             }
         }
 
+        struct info
+        {
+            std::wstring class_name;
+            DWORD n_subkeys;
+            DWORD max_subkey_name_len;
+            DWORD max_class_len;
+            DWORD n_values;
+            DWORD max_value_name_len;
+            DWORD max_value_size;
+            DWORD security_desc_size;
+            PFILETIME last_write_time;
+        };
+
+        info query_info(DWORD class_name_buffer_size = 16)
+        {
+            auto key_info{ info{} };
+            key_info.class_name.resize(class_name_buffer_size, '\0');
+
+            auto ls{ RegQueryInfoKey(
+                m_key,
+                key_info.class_name.data(),
+                &class_name_buffer_size,
+                nullptr,
+                &key_info.n_subkeys,
+                &key_info.max_subkey_name_len,
+                &key_info.max_class_len,
+                &key_info.n_values,
+                &key_info.max_value_name_len,
+                &key_info.max_value_size,
+                &key_info.security_desc_size,
+                key_info.last_write_time
+            ) };
+
+            if (ls != ERROR_SUCCESS)
+            {
+                if (ls == ERROR_MORE_DATA)
+                {
+                    key_info = query_info(class_name_buffer_size * 2);
+                }
+                else
+                {
+                    auto ec{ std::error_code(ls, std::system_category()) };
+                    throw std::system_error(ec, "RegQueryInfoKey() failed");
+                }
+            }
+            
+            return key_info;
+        }
+
+
+
         const HKEY& get() const noexcept { return m_key; }
 
         bool is_open() const noexcept { return m_key; }
