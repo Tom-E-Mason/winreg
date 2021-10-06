d3d2f249540aa941d0d04ee1d3cf40c84ce8290a
Tom Mason <masonworldt@gmail.com>
2021-10-06 21:55:00 +0100

    changed enumerate to for_eachadded test for constants and enums

--- winreg/test.cpp
+++ winreg/test.cpp
@@ -11,6 +11,30 @@ using namespace std::string_literals;
 #define STR(str) str ## s
 #endif
 
+TEST(winreg_test, enums_and_constants)
+{
+    using namespace winreg;
+
+    EXPECT_EQ((REGSAM)access::all_access, KEY_ALL_ACCESS);
+    EXPECT_EQ((REGSAM)access::create_link, KEY_CREATE_LINK);
+    EXPECT_EQ((REGSAM)access::create_sub_key, KEY_CREATE_SUB_KEY);
+    EXPECT_EQ((REGSAM)access::enumerate_sub_keys, KEY_ENUMERATE_SUB_KEYS);
+    EXPECT_EQ((REGSAM)access::execute, KEY_EXECUTE);
+    EXPECT_EQ((REGSAM)access::notify, KEY_NOTIFY);
+    EXPECT_EQ((REGSAM)access::query_value, KEY_QUERY_VALUE);
+    EXPECT_EQ((REGSAM)access::read, KEY_READ);
+    EXPECT_EQ((REGSAM)access::set_value, KEY_SET_VALUE);
+    EXPECT_EQ((REGSAM)access::wow64_32key, KEY_WOW64_32KEY);
+    EXPECT_EQ((REGSAM)access::wow64_64key, KEY_WOW64_64KEY);
+    EXPECT_EQ((REGSAM)access::write, KEY_WRITE);
+
+    EXPECT_EQ(classes_root.get(), HKEY_CLASSES_ROOT);
+    EXPECT_EQ(current_user.get(), HKEY_CURRENT_USER);
+    EXPECT_EQ(local_machine.get(), HKEY_LOCAL_MACHINE);
+    EXPECT_EQ(users.get(), HKEY_USERS);
+    EXPECT_EQ(current_config.get(), HKEY_CURRENT_CONFIG);
+}
+
 TEST(winreg_test, openclose)
 {
     auto path{ STR("software\\asio") };
@@ -31,7 +55,7 @@ TEST(winreg_test, openclose)
     EXPECT_TRUE(info.last_write_time == 0);
 
     auto count{ info.n_subkeys };
-    subkey.enumerate([&count](winreg::string subkey) { --count; return true; });
+    subkey.for_each([&count](winreg::string subkey) { --count; return true; });
 
     EXPECT_TRUE(count == 0);
 
